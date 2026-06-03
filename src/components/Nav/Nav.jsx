c112c6b52d0670532bddc8e8b4d8a6fe9dad1a07
sagar-shrigadi <181492974+sagar-shrigadi@users.noreply.github.com>
2026-06-03 21:05:35 +0530

    feat(Nav): Implement Logout functionality and Update nav links to use NavLink component
    - Conditionally render Login/Logout Nav option based on whether a user is logged in or not
    - Update Global and Chats Nav option to use NavLink component from react-router and add appropriate `to` paths for proper navigation between pages

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,25 +1,60 @@
-import { CircleUserRound, Globe, MessageCircleMore, Power } from "lucide-react";
+import {
+  CircleUserRound,
+  Globe,
+  LogIn,
+  LogOut,
+  MessageCircleMore,
+} from "lucide-react";
+import { useRef } from "react";
+import { NavLink, useNavigate } from "react-router";
 
-const Nav = () => {
+const Nav = ({ user }) => {
+  let navigate = useNavigate();
+  const btnRef = useRef(null);
+  const logoutUser = async () => {
+    btnRef.current.textContent = "Logging out...";
+    localStorage.removeItem("token");
+    navigate("login");
+  };
   return (
     <nav>
-      <ul className="flex justify-between px-2 sm:flex-col sm:py-2 sm:justify-stretch sm:text-xl sm:max-w-fit sm:min-h-dvh">
-        <li className="flex items-center gap-2 cursor-pointer sm:mb-6">
+      <ul className="flex justify-between px-2 sm:flex-col py-2 sm:justify-stretch sm:text-xl sm:max-w-fit sm:min-h-dvh border">
+        <NavLink
+          to="/"
+          className="flex items-center gap-2 cursor-pointer sm:mb-6"
+        >
           <Globe className="size-8" />
           <span className="hidden lg:block">Global</span>
-        </li>
-        <li className="flex items-center gap-2 cursor-pointer">
+        </NavLink>
+
+        <NavLink to="users" className="flex items-center gap-2 cursor-pointer">
           <MessageCircleMore className="size-8" />
           <span className="hidden lg:block">Chats</span>
-        </li>
+        </NavLink>
+
         <li className="flex items-center gap-2 cursor-pointer sm:mt-auto sm:mb-6">
           <CircleUserRound className="size-8" />
           <span className="hidden lg:block">Profile</span>
         </li>
-        <li className=" flex items-center gap-2 cursor-pointer">
-          <Power className="size-8" />
-          <span className="hidden lg:block">Login</span>
-        </li>
+        {user?.id ? (
+          <button
+            className="flex items-center gap-2 cursor-pointer"
+            onClick={logoutUser}
+          >
+            <LogOut className="size-8" />
+            <span className="hidden lg:block" ref={btnRef}>
+              Logout
+            </span>
+          </button>
+        ) : (
+          <NavLink
+            to="login"
+            className="flex items-center gap-2 cursor-pointer"
+          >
+            <LogIn className="size-8" />
+            <span className="hidden lg:block">Login</span>
+          </NavLink>
+        )}
       </ul>
     </nav>
   );
