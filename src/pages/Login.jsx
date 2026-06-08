da4b55d4eb5976c0ef74bf04a224515a42a2da86
sagar-shrigadi <181492974+sagar-shrigadi@users.noreply.github.com>
2026-06-08 14:55:58 +0530

    feat(Login): allow users to login with a demo account
    - add a second form, to allow user to log in without creating an account
    - pre-populate username and password and log the user in
    - set the username and password as env variables
    - create a env.example file, for better reference on what env variables are needed to run the app locally

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,13 @@
 import { useRef, useState } from "react";
 import LogUser from "../service/auth/login";
 import { Link, useNavigate } from "react-router";
+import { CircleUserRound } from "lucide-react";
 
 const Login = () => {
   const [validationError, setValidationError] = useState([]);
   const [credentialsError, setCredentialsError] = useState("");
   const btnRef = useRef(null);
+  const dummyBtnRef = useRef(null);
   let navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -34,10 +36,33 @@ const Login = () => {
       }
     }
   };
+  const handleDummySubmit = async (e) => {
+    e.preventDefault();
+    dummyBtnRef.current.textContent = "Logging in...";
+
+    const res = await LogUser({
+      username: `${import.meta.env.VITE_DEMO_USERNAME}`,
+      password: `${import.meta.env.VITE_DEMO_PASSWORD}`,
+    });
+    if (res.success) {
+      localStorage.setItem("token", res.data);
+      navigate("/");
+    } else {
+      dummyBtnRef.current.textContent = "Log in";
+
+      if (res.message === "Validation Failed!") {
+        setCredentialsError("");
+        setValidationError(res.errors);
+      } else {
+        setCredentialsError(res.message);
+        setValidationError([]);
+      }
+    }
+  };
   return (
     <section className="flex flex-col gap-4 items-center justify-center m-auto p-4 w-dvw max-w-136">
       <h1 className="text-4xl mr-auto">Welcome Back</h1>
-      <form onSubmit={handleSubmit} className="w-full py-5 flex flex-col gap-5">
+      <form onSubmit={handleSubmit} className="w-full mt-5 flex flex-col gap-5">
         <div className="flex-1 flex flex-col ">
           <label htmlFor="username">Username</label>
           <input
@@ -70,20 +95,21 @@ const Login = () => {
           {validationError && validationError.map((e) => <p>{e.message}</p>)}
           {credentialsError && <p>{credentialsError}</p>}
         </div>
-        <div className="flex justify-end gap-5">
-          <Link
-            to="/"
-            className="cursor-pointer hover:bg-gray-800 hover:text-white px-4 py-2 rounded transition-all"
-          >
-            Global Chat
-          </Link>
-          <button
-            ref={btnRef}
-            className="cursor-pointer py-2 px-6 rounded text-white bg-gray-800 hover:bg-gray-700 transition-all"
-          >
-            Log in
-          </button>
-        </div>
+        <button
+          ref={btnRef}
+          className="cursor-pointer py-2 px-6 rounded text-white bg-gray-800 hover:bg-gray-700 transition-all"
+        >
+          Log in
+        </button>
+      </form>
+      <form onSubmit={handleDummySubmit} className="w-full flex">
+        <button
+          ref={dummyBtnRef}
+          className="grow cursor-pointer border border-gray-800 hover:bg-gray-800 hover:text-white px-4 py-2 rounded transition-all flex items-center justify-center gap-6"
+        >
+          <CircleUserRound className="size-6" />
+          Try a Demo Account
+        </button>
       </form>
     </section>
   );
